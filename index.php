08d5c3b489eceeec21f8ea1b1de2d99a00fcb756
LeonardoMtz98 <33941450+LeonardoMtz98@users.noreply.github.com>
2018-04-13 00:14:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
   <title>CRUD: Sistema escolar</title>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="css/estilo2.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
   <meta name="keywords" content="">
   <meta charset="utf-8">
 </head>
